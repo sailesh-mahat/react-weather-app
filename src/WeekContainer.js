ff7254c1e8c85876ee8c2a4d622d9b2e1e1cf6d5
sailesh-mahat <sailesharsenal@gmail.com>
2020-03-03 18:39:37 +0100

    added fetching fromAPI and filtration of data

--- src/WeekContainer.js
+++ src/WeekContainer.js
@@ -1,6 +1,27 @@
 import React from 'react';
+import apiConfig from './apiKeys';
 
 class WeekContainer extends React.Component {
+  state = {
+    fullData: [],
+    dailyData: []
+  }
+
+  componentDidMount = () => {
+    const weatherURL =
+    `http://api.openweathermap.org/data/2.5/forecast?zip=40215&units=imperial&APPID=${apiConfig.owmKey}`
+
+    fetch(weatherURL)
+    .then(res => res.json())
+    .then(data => {
+      const dailyData = data.list.filter(reading => reading.dt_txt.includes("18:00:00"))
+      this.setState({
+        fullData: data.list,
+        dailyData: dailyData
+      }, () => console.log(this.state))
+    })
+  }
+
   render() {
     return (
       <div>
